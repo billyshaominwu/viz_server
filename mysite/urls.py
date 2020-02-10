b82c02e2dbcbb38ead501abc1228d2dc2e8e2847
Alan Ensastegui Salazar <alan98@uw.edu>
2020-02-10 15:13:16 -0800

    Added http handler to return all crimes within a given radius. Currently uses a box instead of circle model for radius

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('mysite/', include('mysite.crimesapp.urls')),
     path('admin/', admin.site.urls),
+    path('crimes/<str:longitude>/<str:latitude>/<str:radius>', views.getAllCrimesWithinR)
 ]
