fd4a88c5872645d0dd6e3c91213ca1fd0222fa17
Billy Wu <billyw@cs.washington.edu>
2020-02-13 00:48:34 -0800

    allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '811$89u40l0dnf#((*rq^yspp#9=i5-sm0%m2!jppa)2-+=hui'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
